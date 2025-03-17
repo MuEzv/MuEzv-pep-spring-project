4f5c6fa50e354f87f9156f6082aaceea6bfb3b4c
Vivian Z <muranzzw@gmail.com>
2025-03-17 16:53:27 +0000

    1.register user

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,13 @@
 package com.example.service;
 
+import com.example.repository.AccountRepository;
+
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    // register account
+    public Account register(Account account){
+        return AccountRepository.save(account);
+    }
 }
