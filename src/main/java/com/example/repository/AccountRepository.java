5b14e516e227f3ad8c04a87fa25dab59c4c973c6
Vivian Z <muranzzw@gmail.com>
2025-03-17 17:18:10 +0000

    v4

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.entity.Account;
+
 public interface AccountRepository extends JpaRepository<Account, Integer>{
 
     boolean existsByUsername(String username);
